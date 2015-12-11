f2624151f9d81e665d316d4c7960a2c6a193d02d
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-12-11 09:39:00 -0500

    /press, /communities, /posts GET methods updated and new .jar files

--- src/main/java/com/theironyard/entities/Press.java
+++ src/main/java/com/theironyard/entities/Press.java
@@ -1,7 +1,22 @@
 package com.theironyard.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by jessicahuffstutler on 12/10/15.
  */
+@Entity
+@Table(name = "pressPosts")
 public class Press {
+
+    public String title;
+
+    public String byline;
+
+    public String link;
+
+    public String imageLink; //or URL
+
+    public String description;
 }
