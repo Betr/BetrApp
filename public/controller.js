5f7e1b749bde070951e7f9119827ee27df1ef003
Kate Wilson <wilsonkate.kw@gmail.com>
2015-12-14 14:47:34 -0500

    about to start braintree

--- public/controller.js
+++ public/controller.js
@@ -72,7 +72,6 @@
               PressService.newPress(pressItem);
               // $location.path('/admin');
             };
-
             vm.getPress = function (pressItem){
               PressService.getPress(pressItem);
             };
@@ -83,8 +82,36 @@
             vm.deletePress = function (pressItem){
               PressService.deletePress(pressItem);
             };
+})
 
-     });
+    .controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
+      $scope.items = ['item1', 'item2', 'item3'];
+        $scope.animationsEnabled = true;
+        $scope.open = function (size) {
+          var modalInstance = $uibModal.open({
+            animation: $scope.animationsEnabled,
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+              items: function () {
+                return $scope.items;
+              }
+            }
+          })
+
+          modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+          }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+          });
+        };
+
+    $scope.toggleAnimation = function () {
+      $scope.animationsEnabled = !$scope.animationsEnabled;
+    };
+  });
 
 
+// ModalInstanceCtrl
  })();
