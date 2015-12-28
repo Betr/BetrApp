dd052dd302c842baebfc7031753ab2e2dfd5dc12
BennettIronYard <bennettiy12@gmail.com>
2015-12-28 14:20:55 -0500

    Login Changed to a PUT

--- src/main/java/com/theironyard/controllers/BetrController.java
+++ src/main/java/com/theironyard/controllers/BetrController.java
@@ -81,7 +81,7 @@ public class BetrController {
         users.delete(id);
     }
 
-    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    @RequestMapping(path = "/login", method = RequestMethod.PUT)
     public User login(HttpSession session, @RequestBody User user) throws Exception {
 
         User currentUser = users.findOneByEmail(user.email);
