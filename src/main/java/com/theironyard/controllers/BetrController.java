b69ad7738088ccd986fa60201f1cf38835fbd018
jessicahuffstutler <jessica.huffstutler@gmail.com>
2016-01-04 14:22:47 -0500

    transaction update and jar

--- src/main/java/com/theironyard/controllers/BetrController.java
+++ src/main/java/com/theironyard/controllers/BetrController.java
@@ -49,7 +49,7 @@ public class BetrController {
     );
 
     @RequestMapping(path = "/transaction/{id}", method = RequestMethod.POST)
-    public Object addTransaction(@RequestBody TransactionParams params, int id){
+    public Object addTransaction(@RequestBody TransactionParams params, @PathVariable("id") int id){
         TransactionRequest request = new TransactionRequest()
                 .amount(new BigDecimal(params.amount))
                 .paymentMethodNonce("fake-valid-nonce")
@@ -67,7 +67,7 @@ public class BetrController {
 
     }
 //    @RequestMapping(path = "/transaction/{id}", method = RequestMethod.PUT)
-//    public void editTransaction(@RequestBody TransactionParams params, int id){
+//    public void editTransaction(@RequestBody TransactionParams params, @PathVariable("id") int id){
 //        Community community = communities.findOne(id);
 //        community.amount.add(new BigDecimal(params.amount));
 //
